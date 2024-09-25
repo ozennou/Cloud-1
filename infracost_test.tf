4598f51b353322ac05db04b009d6fc6cd19e72a7
mohamed amine ozennou <102986762+ozennou@users.noreply.github.com>
2024-09-25 17:52:50 +0100

    Update infracost_test.tf

--- infracost_test.tf
+++ infracost_test.tf
@@ -1,45 +1,95 @@
-provider "google" {
-  region = "us-central1"
-  project = "test"
+// VPC
+resource "google_compute_network" "default" {
+    name = "cloud-1-vpc"
+    auto_create_subnetworks = false
 }
 
-resource "google_compute_instance" "my_instance" {
-  zone = "us-central1-a"
-  name = "test"
+resource "google_compute_subnetwork" "default" {
+    name = "cloud-1-subnet"
+    ip_cidr_range = "10.0.0.0/24"
 
-  machine_type = "n1-standard-32" # <<<<<<<<<< Try changing this to n1-standard-32 to compare the costs
-  network_interface {
-    network = "default"
-    access_config {}
-  }
+    network = google_compute_network.default.self_link
+}
+// Disk for presistence
+resource "google_compute_disk" "default" {
+    name = "cloud-1-disk"
+    type = "pd-standard"
+    zone = var.zone
+    size = 10
+}
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
+resource "google_compute_address" "default" {
+    count = var.instances_count
+    name = "cloud-1-ip-${count.index}"
+}
+
+// Instance
+resource "google_compute_instance" "default" {
+    count = var.instances_count
+    name = "cloud-1-instance-${count.index}"
+    machine_type = var.machine_type
+
+    boot_disk {
+        initialize_params {
+            image = var.instance_image
+            size = 10
+        }
     }
-  }
 
-  scheduling {
-    preemptible = true
-  }
+    attached_disk {
+        source = google_compute_disk.default.self_link
+        device_name = "cloud-1-disk"
+    }
 
-  guest_accelerator {
-    type = "nvidia-tesla-p4" # <<<<<<<<<< Try changing this to nvidia-tesla-p4 to compare the costs
-    count = 4
-  }
+    network_interface {
+        network = google_compute_network.default.self_link
+        subnetwork = google_compute_subnetwork.default.self_link
+        access_config {
+            nat_ip = google_compute_address.default[count.index].address
+        }
+    }
 
-  labels = {
-    environment = "production"
-    service = "web-app"
-  }
+    metadata_startup_script = file("./scripts/startup.sh")
+
+    provisioner "file" {
+        source = "../srcs"
+        destination = "/home/amine/srcs"
+        connection {
+            type = "ssh"
+            user = "amine"
+            private_key = file("./ssh_key")
+            host = self.network_interface[0].access_config[0].nat_ip
+        }
+    }
+    tags = ["ssh-access", "https-access"]
+
+    lifecycle {
+        create_before_destroy = true
+    }
 }
 
-resource "google_cloudfunctions_function" "my_function" {
-  runtime = "nodejs20"
-  name = "test"
-  available_memory_mb = 512
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.default.self_link
 
-  labels = {
-    environment = "Prod"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
   }
+
+  source_ranges = ["0.0.0.0/0"] 
+  target_tags    = ["ssh-access"]
+}
+
+resource "google_compute_firewall" "allow_https" {
+  name    = "allow-https"
+  network = google_compute_network.default.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] 
+    target_tags    = ["https-access"]
 }
